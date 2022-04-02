0d318b747a2e62013be2b4de26bfa0fe65b09e64
Mahnoor Hashmi <91974252+mahnoorr1@users.noreply.github.com>
2022-04-02 18:23:18 +0500

    initialized

--- lib/Views/home_screen.dart
+++ lib/Views/home_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:kiulinks_app/utilities/SizeConfig.dart';
 class home extends StatelessWidget {
 
 
@@ -6,10 +7,41 @@ class home extends StatelessWidget {
   Widget build(BuildContext context) {
     return Stack(
       children: [
-        ListTile(
-          title: Text(
-            "Hola! Userio",
-            style: Theme.of(context).textTheme.headline4,
+        Container(
+          height: MediaQuery.of(context).size.height*0.2,
+          margin: EdgeInsets.only(top: MediaQuery.of(context).size.height*0.02),
+          child: Row(
+            children:[
+              SizedBox(width: MediaQuery.of(context).size.width*0.45,),
+              Container(
+                margin: EdgeInsets.only(top: MediaQuery.of(context).size.height*0.08),
+                width: MediaQuery.of(context).size.width*0.35,
+                child: Column(
+                  children: [
+                Text(
+                  "Hola! Usuario",
+                  style: TextStyle(
+                    fontSize: 4* SizeConfig.blockSizeHorizontal,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+                Text(
+                "       Buenos días!",
+                  style: TextStyle(
+                    fontSize: 3* SizeConfig.blockSizeHorizontal,
+                  ),
+                ),
+
+              ]),),
+              SizedBox(
+                width: MediaQuery.of(context).size.width*0.14,
+                child:CircleAvatar(
+                  radius: MediaQuery.of(context).size.width*0.08,
+                  backgroundImage: AssetImage("assets/profile icon.png"),
+                  backgroundColor: Colors.blue,
+                ),
+              )
+            ]
           ),
         )
       ],
