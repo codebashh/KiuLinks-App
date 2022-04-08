cd9092da6c3ba4a9ed7989d582801ac3dd35db14
Mahnoor Hashmi <91974252+mahnoorr1@users.noreply.github.com>
2022-04-08 22:42:17 +0500

    updated

--- lib/Views/home_screen.dart
+++ lib/Views/home_screen.dart
@@ -1,9 +1,19 @@
 import 'package:flutter/material.dart';
+import 'package:kiulinks_app/Views/schedule_screen.dart';
 import 'package:kiulinks_app/utilities/SizeConfig.dart';
 
 class home extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+
+    //defining dynamic values
+    double width = MediaQuery.of(context).size.width;
+    double height = MediaQuery.of(context).size.height;
+    double smallCardWidth = width * 0.42;
+    double smallCardHeight = height * 0.22;
+    double cardHorizontalMargin = width * 0.05;
+    double  largeCardHeight = height * 0.15;
+
     TextStyle heading1 = TextStyle(       //used for two main header texts
           fontSize: 6.2 * SizeConfig.blockSizeHorizontal,
         fontWeight: FontWeight.w800,
@@ -15,22 +25,23 @@ class home extends StatelessWidget {
     TextStyle heading3 = TextStyle(       //used for subtext in cards and screen header
         fontSize: 4.2 * SizeConfig.blockSizeHorizontal,
         color: Theme.of(context).primaryColor);
+
     return Scaffold(
       backgroundColor: Theme.of(context).backgroundColor,
         body: Stack(
       children: [
         //header , user profile and greeting
         Container(
-          height: MediaQuery.of(context).size.height * 0.2,
+          height: height * 0.2,
           margin:
-              EdgeInsets.only(top: MediaQuery.of(context).size.height * 0.005),
+              EdgeInsets.only(top: height * 0.005),
           child: Row(children: [
             SizedBox(
-              width: MediaQuery.of(context).size.width * 0.45,
+              width: width * 0.45,
             ),
             Container(
               margin: EdgeInsets.only(
-                  top: MediaQuery.of(context).size.height * 0.08),
+                  top: height * 0.08),
               // width: MediaQuery.of(context).size.width * 0.45,
               child:
                   Column(crossAxisAlignment: CrossAxisAlignment.end, children: [
@@ -52,11 +63,11 @@ class home extends StatelessWidget {
             ),
             Container(
               margin: EdgeInsets.only(
-                left: MediaQuery.of(context).size.width * 0.02,
+                left: width * 0.02,
               ),
-              width: MediaQuery.of(context).size.width * 0.14,
+              width: width * 0.14,
               child: CircleAvatar(
-                radius: MediaQuery.of(context).size.width * 0.08,
+                radius: width * 0.08,
                 backgroundImage: AssetImage("assets/profile icon.png"),
                 backgroundColor: Colors.blue,
               ),
@@ -66,10 +77,10 @@ class home extends StatelessWidget {
 
         //subheading for route choosing
         Container(
-          height: MediaQuery.of(context).size.height * 0.15,
+          height: height * 0.15,
           margin: EdgeInsets.only(
-              top: MediaQuery.of(context).size.height * 0.18,
-              left: MediaQuery.of(context).size.width * 0.05),
+              top: height * 0.18,
+              left:  cardHorizontalMargin),
           child: Column(
             crossAxisAlignment: CrossAxisAlignment.start,
             children: [
@@ -86,20 +97,20 @@ class home extends StatelessWidget {
           ),
         ),
         SizedBox(
-          height: MediaQuery.of(context).size.height * 0.05,
+          height: height * 0.05,
         ),
 
         //Join and Schedule class route cards
         Row(
           children: [
             GestureDetector(
-              onTap: () => {},
+              onTap: () => Navigator.of(context).push(MaterialPageRoute(builder: (context)=>schedule())),
               child: Container(
                 margin: EdgeInsets.only(
-                    top: MediaQuery.of(context).size.height * 0.27,
-                    left: MediaQuery.of(context).size.width * 0.05),
-                height: MediaQuery.of(context).size.height * 0.22,
-                width: MediaQuery.of(context).size.width * 0.42,
+                    top: height * 0.27,
+                    left: cardHorizontalMargin),
+                height: smallCardHeight,
+                width: smallCardWidth,
                 decoration: BoxDecoration(
                     borderRadius: BorderRadius.circular(30),
                     color: Colors.white),
@@ -109,12 +120,12 @@ class home extends StatelessWidget {
                       height: 10,
                     ),
                     CircleAvatar(
-                      radius: MediaQuery.of(context).size.width * 0.1,
+                      radius: width * 0.1,
                       backgroundColor: Theme.of(context).backgroundColor,
                       child: Container(
                           child: Image.asset(
                         "assets/live.png",
-                        width: MediaQuery.of(context).size.width * 0.13,
+                        width: width * 0.13,
                       )),
                     ),
                     Text(
@@ -126,7 +137,7 @@ class home extends StatelessWidget {
                       height: 5,
                     ),
                     SizedBox(
-                        width: MediaQuery.of(context).size.width * 0.35,
+                        width: width * 0.35,
                         child: Text(
                           "Ingresa a tu próxima clase",
                           style: heading3,
@@ -138,16 +149,16 @@ class home extends StatelessWidget {
               ),
             ),
             SizedBox(
-              width: MediaQuery.of(context).size.width * 0.06,
+              width: width * 0.06,
             ),
             GestureDetector(
               onTap: () => {},
               child: Container(
                 margin: EdgeInsets.only(
-                    top: MediaQuery.of(context).size.height * 0.27,
-                    right: MediaQuery.of(context).size.width * 0.05),
-                height: MediaQuery.of(context).size.height * 0.22,
-                width: MediaQuery.of(context).size.width * 0.42,
+                    top: height * 0.27,
+                    right: cardHorizontalMargin),
+                height: smallCardHeight,
+                width: smallCardWidth,
                 decoration: BoxDecoration(
                     borderRadius: BorderRadius.circular(30),
                     color: Colors.white),
@@ -157,12 +168,12 @@ class home extends StatelessWidget {
                       height: 10,
                     ),
                     CircleAvatar(
-                      radius: MediaQuery.of(context).size.width * 0.1,
+                      radius: width * 0.1,
                       backgroundColor: Theme.of(context).backgroundColor,
                       child: Container(
                           child: Image.asset(
                         "assets/gift.png",
-                        width: MediaQuery.of(context).size.width * 0.13,
+                        width: width * 0.13,
                       )),
                     ),
                     Text(
@@ -174,7 +185,7 @@ class home extends StatelessWidget {
                       height: 5,
                     ),
                     SizedBox(
-                        width: MediaQuery.of(context).size.width * 0.35,
+                        width: width * 0.35,
                         child: Text(
                           "Nuestros servicios gratis",
                           style: heading3,
@@ -187,88 +198,89 @@ class home extends StatelessWidget {
             ),
           ],
         ),
-        GestureDetector(
-          onTap: () => {},
-          child: Container(
+        Container(
               margin: EdgeInsets.only(
-                  top: MediaQuery.of(context).size.height * 0.51,
-                  left: MediaQuery.of(context).size.width * 0.05),
+                  top: height * 0.51,
+                  left: cardHorizontalMargin),
               child: Text(
                 "Conectar",
                 //style: Theme.of(context).textTheme.headline1,
                 style: heading1,
               )),
-        ),
-        Container(
-          height: MediaQuery.of(context).size.height * 0.15,
-          decoration: BoxDecoration(
-              borderRadius: BorderRadius.circular(30), color: Colors.white),
-          margin: EdgeInsets.only(
-              top: MediaQuery.of(context).size.height * 0.57,
-              left: MediaQuery.of(context).size.width * 0.05,
-              right: MediaQuery.of(context).size.width * 0.05),
-          child: Row(children: [
-            SizedBox(
-              width: MediaQuery.of(context).size.width * 0.06,
-            ),
-            CircleAvatar(
-              radius: MediaQuery.of(context).size.width * 0.1,
-              backgroundColor: Theme.of(context).backgroundColor,
-              child: Container(
-                  child: Image.asset(
-                "assets/community.png",
-                width: MediaQuery.of(context).size.width * 0.13,
-              )),
-            ),
-            SizedBox(
-              width: MediaQuery.of(context).size.width * 0.06,
-            ),
-            Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                Text(
-                  "Comunidad",
-                  style: heading2,
-                  //style: Theme.of(context).textTheme.headline2,
-                ),
-                Container(
-                  width: MediaQuery.of(context).size.width * 0.5,
-                  child: Text(
-                    "Acelera tu aprendizaje",
-                    style: heading3,
-                    //style: Theme.of(context).textTheme.headline5,
+
+        GestureDetector(
+          onTap: ()=> {},
+          child: Container(
+            height:  largeCardHeight,
+            decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(30), color: Colors.white),
+            margin: EdgeInsets.only(
+                top: height * 0.57,
+                left: cardHorizontalMargin,
+                right: cardHorizontalMargin),
+            child: Row(children: [
+              SizedBox(
+                width: width * 0.06,
+              ),
+              CircleAvatar(
+                radius: width * 0.1,
+                backgroundColor: Theme.of(context).backgroundColor,
+                child: Container(
+                    child: Image.asset(
+                  "assets/community.png",
+                  width: width * 0.13,
+                )),
+              ),
+              SizedBox(
+                width: width * 0.06,
+              ),
+              Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text(
+                    "Comunidad",
+                    style: heading2,
+                    //style: Theme.of(context).textTheme.headline2,
                   ),
-                ),
-              ],
-            )
-          ]),
+                  Container(
+                    width: width * 0.5,
+                    child: Text(
+                      "Acelera tu aprendizaje",
+                      style: heading3,
+                      //style: Theme.of(context).textTheme.headline5,
+                    ),
+                  ),
+                ],
+              )
+            ]),
+          ),
         ),
         GestureDetector(
           onTap: () => {},
           child: Container(
-            height: MediaQuery.of(context).size.height * 0.15,
+            height: largeCardHeight,
             decoration: BoxDecoration(
                 borderRadius: BorderRadius.circular(30), color: Colors.white),
             margin: EdgeInsets.only(
-                top: MediaQuery.of(context).size.height * 0.75,
-                left: MediaQuery.of(context).size.width * 0.05,
-                right: MediaQuery.of(context).size.width * 0.05),
+                top: height * 0.75,
+                left: cardHorizontalMargin,
+                right: cardHorizontalMargin),
             child: Row(children: [
               SizedBox(
-                width: MediaQuery.of(context).size.width * 0.06,
+                width: width * 0.06,
               ),
               CircleAvatar(
-                radius: MediaQuery.of(context).size.width * 0.1,
+                radius: width * 0.1,
                 backgroundColor: Theme.of(context).backgroundColor,
                 child: Container(
                     child: Image.asset(
                   "assets/chat.png",
-                  width: MediaQuery.of(context).size.width * 0.13,
+                  width: width * 0.13,
                 )),
               ),
               SizedBox(
-                width: MediaQuery.of(context).size.width * 0.06,
+                width: width * 0.06,
               ),
               Column(
                 mainAxisAlignment: MainAxisAlignment.center,
@@ -280,7 +292,7 @@ class home extends StatelessWidget {
                     //style: Theme.of(context).textTheme.headline2,
                   ),
                   Container(
-                    width: MediaQuery.of(context).size.width * 0.55,
+                    width: width * 0.55,
                     child: Text(
                       "Hablar con un representante",
                       style: heading3,
--- lib/Views/schedule_screen.dart
+++ lib/Views/schedule_screen.dart
@@ -7,12 +7,26 @@ class schedule extends StatefulWidget {
 class _scheduleState extends State<schedule> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: SingleChildScrollView(
-        child: Column(
-          children: [
+    double height = MediaQuery.of(context).size.height;
+    double width = MediaQuery.of(context).size.width;
+    return Material(
+      child: Scaffold(
+        backgroundColor: Theme.of(context).primaryColor,
+        body: SingleChildScrollView(
+          child: Column(
+            children: [
+              Container(
+                width: double.infinity,
+                height: height * 0.3,
+                decoration: BoxDecoration(
+                image: DecorationImage(
+                    image: AssetImage("assets/schedule image.png"),
+                ),
+              ),
 
-          ],
+            ),
+            ],
+          ),
         ),
       ),
     );
